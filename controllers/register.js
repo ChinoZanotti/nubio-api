5bd3d27c7363e6fb48d8abb8fa23e1cf6d60e227
Chino Zanotti <zanotti@arsis.com.py>
2026-09-25 16:18:17 -0300

    valida longitud minima de contraseña en registro

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,15 @@
 export const handleRegister = async (req, res, db, bcrypt) => {
     const { email, name, password } = req.body
     const saltRounds = 10
+
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission')
     }
+
+    if (password.length < 10) {
+        return res.status(400).json('la contraseña debe tener al menos 10 caracteres')
+    }
+
     try {
         const hash = await bcrypt.hash(password, saltRounds)
 
